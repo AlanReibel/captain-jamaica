c3b50f45b78bde92435268588d60c35d6b2fb48e
Alan Reibel <reibeltel@gmail.com>
2024-06-18 14:53:05 +0200

    added bullet flash effect

--- src/objects/Bullet.js
+++ src/objects/Bullet.js
@@ -1,3 +1,5 @@
+import { ScaleModes } from "phaser";
+
 export class Bullet extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, 'bullet');
@@ -10,6 +12,8 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
     }
 
     fire(x, y,direction) {
+
+        this.bulletFlash( x, y);
         this.body.reset(x, y);
         this.body.setAllowGravity(false);
         this.setActive(true);
@@ -18,6 +22,7 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
             ? 1
             : -1;
         this.setVelocityX(1000 * directionX);
+
     }
 
     update() {
@@ -29,5 +34,30 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
 
     animationComplete(animation, frame) {
         this.destroy();
+    }
+
+    bulletFlash( x, y) {
+        let graphics = this.scene.add.graphics();
+        graphics.fillStyle(0xffffff, 1); // Color blanco, opacidad completa
+        graphics.fillCircle(x, y, 15); // Círculo blanco de 50px de diámetro
+        // Convertir el gráfico en una textura
+        graphics.generateTexture('flash', 50, 50); // Generar una textura de 100x100
+    
+        // Crear un sprite para el destello en la misma posición que la bala
+        let flash = this.scene.add.sprite(this.scene.player.x, this.scene.player.y, 'flash');
+
+        // Definir una animación para el destello (opcional)
+        this.scene.tweens.add({
+            targets: graphics,
+            x: x,
+            y: y,
+            // scale: { value: 0, duration: 200, ease: 'Linear' }, // Escalar el destello a 2x en 200ms
+            scale: 0,
+            opacity: 0,
+            duration: 50,
+            // alpha: { value: 0, duration: 500, ease: 'Linear' }, // Desvanecer el destello a 0 en 200ms
+            // duration: 100, // Duración de la animación (milisegundos)
+            onComplete: () => { graphics.destroy(); } // Destruir el destello al finalizar la animación
+        });
     }
 }
