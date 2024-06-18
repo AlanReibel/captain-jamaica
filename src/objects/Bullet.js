80e7117461046c5aa96dad77c9ba41c45aa94770
Alan Reibel <reibeltel@gmail.com>
2024-06-18 15:14:36 +0200

    flash animation bloom added

--- src/objects/Bullet.js
+++ src/objects/Bullet.js
@@ -6,7 +6,7 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
         this.scene = scene;
         this.scene.physics.world.enable(this);
         this.scene.add.existing(this);
-    
+        // this.postFX.addBloom(0xffffff, 1, 1, 0.5, 1);
         this.setCollideWorldBounds(true);
         this.on('animationcomplete', this.animationComplete, this);
     }
@@ -38,26 +38,20 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
 
     bulletFlash( x, y) {
         let graphics = this.scene.add.graphics();
-        graphics.fillStyle(0xffffff, 1); // Color blanco, opacidad completa
-        graphics.fillCircle(x, y, 15); // Círculo blanco de 50px de diámetro
-        // Convertir el gráfico en una textura
-        graphics.generateTexture('flash', 50, 50); // Generar una textura de 100x100
+        graphics.fillStyle(0xffffff, 1); 
+        graphics.fillCircle(x, y, 15); 
+        graphics.postFX.addBloom(0xffffff, 1, 1, 1, 2);
     
-        // Crear un sprite para el destello en la misma posición que la bala
-        let flash = this.scene.add.sprite(this.scene.player.x, this.scene.player.y, 'flash');
-
-        // Definir una animación para el destello (opcional)
+        // graphics.postFX.addBloom();
         this.scene.tweens.add({
             targets: graphics,
             x: x,
             y: y,
-            // scale: { value: 0, duration: 200, ease: 'Linear' }, // Escalar el destello a 2x en 200ms
             scale: 0,
             opacity: 0,
-            duration: 50,
-            // alpha: { value: 0, duration: 500, ease: 'Linear' }, // Desvanecer el destello a 0 en 200ms
-            // duration: 100, // Duración de la animación (milisegundos)
-            onComplete: () => { graphics.destroy(); } // Destruir el destello al finalizar la animación
+            duration: 100,
+            onComplete: () => { graphics.destroy(); } 
+            // Destruir el destello al finalizar la animación
         });
     }
 }
