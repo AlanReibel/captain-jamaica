6894eaca87acd4eaf0cd7032c558a6d09dffd568
Alan Reibel <reibeltel@gmail.com>
2024-07-24 14:24:26 +0200

    dist deletion

--- dist/assets/tiles/greenTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="greenTiles" tilewidth="32" tileheight="32" spacing="16" margin="16" tilecount="60" columns="10">
- <image source="greenTiles.png" width="496" height="304"/>
-</tileset>
